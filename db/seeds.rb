e1a66c994c136ee8b2b330d0d1f946e6e021f4ab
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-15 21:32:50 +0300

    Migrate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+AdminUser.create!(email: 'barhvictor@gmail.com', password: 'railsrails', password_confirmation: 'railsrails') if Rails.env.development?
