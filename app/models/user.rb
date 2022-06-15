eb7ce95b006792aaa03fb7e013c490b4c10df6a2
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-16 00:16:52 +0300

    Set a role to a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,37 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
+  belongs_to :role
+  after_initialize :set_default_role
 
   validates :name, presence: true
 
+  
+  def admin?
+    role.name == 'admin'
+  end
+  
+  def super_admin?
+    role.name == 'super_admin'
+  end
+  
+  def has_permission?(action, resource)
+    role.has_permission?(action, resource) if role
+  end
+  
+  def update_role!(role_name)
+    self.role = Role.find_or_create_by(name: role_name)
+    self.save!
+  end
+
+
   def generate_jwt
     JWT.encode({ id: id, exp: 30.days.from_now.to_i }, 'vicSecret')
+  end
+
+  private
+
+  def set_default_role
+    self.role ||= Role.find_by(name: 'user') if self.role.nil?
   end
 end
