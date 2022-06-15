868e9447f05516a744578bed3cdf46aaa3dffc1f
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-15 21:39:13 +0300

    Create test for user model

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :admin_user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    password_confirmation { password }
+    first_name { Faker::Name.first_name }
   end
 end
