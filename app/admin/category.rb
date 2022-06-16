67bbd3b64ed7fd42fc1be3b07a6e9a0c704fccc6
vic778 <victoiremmanuelbarh@gmail.com>
2022-06-16 13:31:10 +0300

    Associate the user_id and make require the name

---
+++ app/admin/category.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register Category do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :name, :user_id, :sub_categories_attributes => [:id, :name, :_destroy]
+  #
+  # or
+  #
+
+  # end
+end
