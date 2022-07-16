52b1a096daacd963a15e3b68866391a8a19b2075
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-16 13:34:28 +0300

    Create cat before validate the sub

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -3,4 +3,5 @@ class SubCategory < ApplicationRecord
   has_many :drawers
 
   validates :category_id, presence: true, uniqueness: { scope: :name }
+  validates :name, presence: true, uniqueness: true
 end
