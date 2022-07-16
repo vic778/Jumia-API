66ab68a272f45623590e8391ae413ccd61204b4d
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-16 10:58:56 +0300

    Ignore pundit

--- spec/policies/category_policy_spec.rb
+++ spec/policies/category_policy_spec.rb
@@ -1,27 +1,27 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe CategoryPolicy, type: :policy do
-  let(:user) { User.new }
+# RSpec.describe CategoryPolicy, type: :policy do
+#   let(:user) { User.new }
 
-  subject { described_class }
+#   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+#   permissions ".scope" do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+#   permissions :show? do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+#   permissions :create? do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+#   permissions :update? do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-end
+#   permissions :destroy? do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
+# end
