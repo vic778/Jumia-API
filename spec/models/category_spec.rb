13ba49346b8a91a17c51795cda165b2538818d85
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-16 13:58:58 +0300

    Fix linter err

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it {should belong_to(:user)}
-  it {is_expected.to validate_presence_of(:name)}
-
+  it { should belong_to(:user) }
+  it { is_expected.to validate_presence_of(:name) }
 
   describe 'factory validates' do
     it 'should validate factory' do
