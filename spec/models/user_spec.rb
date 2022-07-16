13a4ed15ef707e924a399872d4c3d47585be7d36
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-16 14:00:20 +0300

    Fix linter errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it {should belong_to(:role)}
-  it {is_expected.to validate_presence_of(:name)}
-  it {is_expected.to validate_presence_of(:email)}
-  it {is_expected.to validate_presence_of(:password)}
-  it {is_expected.to validate_presence_of(:password_confirmation)}
-  
+  it { should belong_to(:role) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_presence_of(:password_confirmation) }
 
   describe 'factory validates' do
     it 'should validate factory' do
@@ -28,5 +27,4 @@ RSpec.describe User, type: :model do
       expect(build(:user, password_confirmation: nil)).to be_invalid
     end
   end
-
 end
