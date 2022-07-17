7dde3537f1cd37247bdc9366a6cb557bb91ab942
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-17 08:57:06 +0300

    Ignore get user because of the route

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "when as a admin" do
+    let!(:user) { create(:user, :admin) }
+
+
+    describe "GET /users" do
+      it "returns a list of users" do
+        get users_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    # describe "GET /users/:id" do
+    #   it "returns a user" do
+    #     get user_path
+    #     expect(response).to have_http_status(200)
+    #   end
+    # end
+
+    describe "PUT /users/:id" do
+      it "updates a user" do
+        put user_path(user)
+        expect(response).to have_http_status(200)
+      end
+    end
+    
   end
+  
 end
