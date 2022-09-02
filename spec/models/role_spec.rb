4ca708137274110516c661a779d7ac66842f0e43
vic778 <victoiremmanuelbarh@gmail.com>
2022-09-02 17:10:06 +0300

    Validate presence of name

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:users) }
+  it { is_expected.to validate_presence_of(:name) }
+
+  describe 'factory validates' do
+    it 'should validate factory' do
+      @role = FactoryBot.create(:role)
+      expect(@role).to be_valid
+    end
+  end
 end
