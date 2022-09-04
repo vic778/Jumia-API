17c921ed910f4fbbd860ba7687014161c6247a97
vic778 <victoiremmanuelbarh@gmail.com>
2022-09-04 17:15:55 +0300

    Create delete path

---
+++ spec/requests/api/sub_categories_spec.rb
@@ -0,0 +1,100 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/sub_categories', type: :request do
+  path '/api/categories/1/sub_categories' do
+    get 'Retrieves all sub_categories' do
+      tags 'SubCategories'
+      produces 'application/json'
+      response '200', 'sub_categories found' do
+        schema type: :array,
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   name: { type: :string },
+                   category_id: { type: :integer },
+                   created_at: { type: :string },
+                   updated_at: { type: :string }
+                 }
+               }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/categories/1/sub_categories/1' do
+    get 'Retrieves a sub_category' do
+      tags 'SubCategories'
+      produces 'application/json'
+      response '200', 'sub_category found' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 category_id: { type: :integer },
+                 created_at: { type: :string },
+                 updated_at: { type: :string }
+               }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/categories/1/sub_categories' do
+    post 'Creates a sub_category' do
+      tags 'SubCategories'
+      consumes 'application/json'
+      security [Bearer: {}]
+      parameter name: :sub_category, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          category_id: { type: :integer }
+        },
+        required: %w[name category_id]
+      }
+      response '201', 'sub_category created' do
+        let(:sub_category) { { name: 'FOOD', category_id: 1 } }
+        run_test!
+      end
+      response '422', 'invalid request' do
+        let(:sub_category) { { name: 'FOOD' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/categories/1/sub_categories/1' do
+    put 'Updates a sub_category' do
+      tags 'SubCategories'
+      consumes 'application/json'
+      security [Bearer: {}]
+      parameter name: :sub_category, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          category_id: { type: :integer }
+        },
+        required: %w[name category_id]
+      }
+      response '204', 'sub_category updated' do
+        let(:sub_category) { { name: 'FOOD', category_id: 1 } }
+        run_test!
+      end
+      response '422', 'invalid request' do
+        let(:sub_category) { { name: 'FOOD' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/categories/1/sub_categories/1' do
+    delete 'Deletes a sub_category' do
+      tags 'SubCategories'
+      security [Bearer: {}]
+      response '204', 'sub_category deleted' do
+        run_test!
+      end
+    end
+  end
+end
