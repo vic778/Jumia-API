5c93f833452871944902b1b9b6582953ce9e05af
vic778 <victoiremmanuelbarh@gmail.com>
2022-09-04 16:37:47 +0300

    Remove the auth

--- config/initializers/rswag-ui.rb
+++ config/initializers/rswag-ui.rb
@@ -7,8 +7,9 @@ Rswag::Ui.configure do |c|
   # then the list below should correspond to the relative paths for those endpoints
 
   c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
+  # c.swagger_endpoint '/api-docs/v1/swagger.json', 'API V1 Docs'
 
   # Add Basic Auth in case your API is private
   # c.basic_auth_enabled = true
-  # c.basic_auth_credentials 'username', 'password'
+  # c.basic_auth_credentials 'barhvictor', 'password'
 end
