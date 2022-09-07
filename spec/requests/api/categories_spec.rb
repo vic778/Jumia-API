d5757172518ca77f453f59205888af4e93a1b467
vic778 <victoiremmanuelbarh@gmail.com>
2022-09-07 10:24:02 +0300

    Auth user before ny action

--- spec/requests/api/categories_spec.rb
+++ spec/requests/api/categories_spec.rb
@@ -1,6 +1,69 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/categories', type: :request do
+  context 'when as an admin' do
+    path '/api/categories' do
+      post 'Creates a category' do
+        tags 'Categories'
+        security [bearer_auth: []]
+        consumes 'application/json'
+        parameter name: :category, in: :body, schema: {
+          type: :object,
+          properties: {
+            category: {
+              type: :object,
+              properties: {
+                name: { type: :string }
+              },
+              required: %w[name]
+            }
+          },
+          required: ['category']
+        }
+        response '201', 'category created' do
+          let(:category) { { category: { name: 'victor' } } }
+          run_test!
+        end
+      end
+    end
+
+    path '/api/categories/{id}' do
+      put 'Updates a category' do
+        tags 'Categories'
+        security [bearer_auth: []]
+        consumes 'application/json'
+        parameter name: :id, in: :path, type: :string
+        parameter name: :category, in: :body, schema: {
+          type: :object,
+          properties: {
+            category: {
+              type: :object,
+              properties: {
+                name: { type: :string }
+              },
+              required: %w[name]
+            }
+          },
+          required: ['category']
+        }
+        response '200', 'category updated' do
+          let(:category) { { category: { name: 'victor' } } }
+          run_test!
+        end
+      end
+
+      delete 'Deletes a category' do
+        tags 'Categories'
+        security [bearer_auth: []]
+        consumes 'application/json'
+        parameter name: :id, in: :path, type: :string
+        response '204', 'category deleted' do
+          run_test!
+        end
+      end
+    end
+  end
+
   context 'when as a user' do
     path '/api/categories' do
       get 'Retrieves all categories' do
@@ -21,7 +84,7 @@ RSpec.describe 'api/categories', type: :request do
         end
       end
     end
-
+  
     path '/api/categories/{id}' do
       get 'Retrieves a category' do
         tags 'Categories'
