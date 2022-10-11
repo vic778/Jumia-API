8a8f6efcd468c4b4aaff902c1f51138c46c6fa8c
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-11 11:36:29 +0300

    Generate frineldy for subcategories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_082930) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_083552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -162,7 +162,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_082930) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
     t.index ["category_id"], name: "index_sub_categories_on_category_id"
+    t.index ["slug"], name: "index_sub_categories_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
