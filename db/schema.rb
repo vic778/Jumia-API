6163b67f3f29ab097dba2a18fdfab988883701bb
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-11 11:09:25 +0300

    Generate frinly for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_073431) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_080749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -127,7 +127,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_073431) do
     t.bigint "drawer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
     t.index ["drawer_id"], name: "index_posts_on_drawer_id"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   create_table "roles", force: :cascade do |t|
