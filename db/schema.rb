897b4979e824d267368d417adcf16ff115876354
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-11 11:31:25 +0300

    Create fridnly table where prevoius changed will be keeping until

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_080749) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_082930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_080749) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.index ["sub_category_id"], name: "index_drawers_on_sub_category_id"
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "key_features", force: :cascade do |t|
