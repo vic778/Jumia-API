85640405f0d1516f50826fadc08639dd6954980c
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-11 10:39:02 +0300

    config frineldy and migrate the migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_114003) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_073431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_20_114003) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
