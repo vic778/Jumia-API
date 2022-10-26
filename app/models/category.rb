4797b79b70a6b8d348bb8da69e805446c779f72d
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-26 22:42:42 +0400

    Create slug for cnaditens

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,18 @@
 class Category < ApplicationRecord
   extend FriendlyId
-  friendly_id :name, use: %i[slugged finders]
+  friendly_id :slug_candidates, use: %i[slugged finders]
   belongs_to :user
   has_many :sub_categories, dependent: :destroy
   has_many :drawers, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  def slug_candidates
+    [
+      :name,
+      %i[name]
+      # [:name, :street, :city],
+      # [:name, :street_number, :street, :city]
+    ]
+  end
 end
