5b89b20d17be85f282b4963c06f72e45bc666f5b
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-27 13:47:40 +0400

    Add delete method

--- spec/requests/api/categories_spec.rb
+++ spec/requests/api/categories_spec.rb
@@ -65,67 +65,70 @@ RSpec.describe 'api/categories', type: :request do
           required: ['category']
         }
         response '200', 'category updated' do
-            let(:role) { create(:role, name: 'user') }
-            let(:user) { create(:user, role: role) }
-            let(:Authorization) { "Bearer #{user.generate_jwt}" }
-            let(:category) { { category: { name: 'Phones' } } }
-            run_test! do |response|
-              data = JSON.parse(response.body)
-              expect(data['name']) == ('Phones')
-            end
+          let(:role) { create(:role, name: 'user') }
+          let(:user) { create(:user, role: role) }
+          let(:Authorization) { "Bearer #{user.generate_jwt}" }
+          let(:category) { { category: { name: 'Phones' } } }
+          run_test! do |response|
+            data = JSON.parse(response.body)
+            expect(data['name']) == ('Phones')
+          end
         end
       end
     end
 
-    # #   delete 'Deletes a category' do
-    # #     tags 'Categories'
-    # #     security [bearer_auth: []]
-    # #     consumes 'application/json'
-    # #     parameter name: :id, in: :path, type: :string
-    # #     response '204', 'category deleted' do
-    # #       run_test!
-    # #     end
-    # #   end
-    # end
+    delete 'Deletes a category' do
+      tags 'Categories'
+      security [bearer_auth: []]
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :string
+      response '204', 'category deleted' do
+        run_test!
+      end
+    end
   end
 
-  #   context 'when as a user' do
-  #     path '/api/categories' do
-  #       get 'Retrieves all categories' do
-  #         tags 'Categories'
-  #         produces 'application/json'
-  #         response '200', 'categories found' do
-  #           schema type: :array,
-  #                  items: {
-  #                    type: :object,
-  #                    properties: {
-  #                      id: { type: :integer },
-  #                      name: { type: :string },
-  #                      created_at: { type: :string },
-  #                      updated_at: { type: :string }
-  #                    }
-  #                  }
-  #           run_test!
-  #         end
-  #       end
-  #     end
+  context 'when as a user' do
+    # before do
+    #  #allow model to use friendly_id and find by slug
+    #   allow(Category).to receive(:id).and_return(Category)
+    # end
+    path '/api/categories' do
+      get 'Retrieves all categories' do
+        tags 'Categories'
+        produces 'application/json'
+        response '200', 'categories found' do
+          schema type: :array,
+                 items: {
+                   type: :object,
+                   properties: {
+                     id: { type: :integer },
+                     name: { type: :string },
+                     created_at: { type: :string },
+                     updated_at: { type: :string }
+                   }
+                 }
+          run_test!
+        end
+      end
+    end
 
-  #     # path '/api/categories/{id}' do
-  #     #   get 'Retrieves a category' do
-  #     #     tags 'Categories'
-  #     #     produces 'application/json'
-  #     #     parameter name: :id, in: :path, type: :string
-  #     #     response '200', 'category found' do
-  #     #       schema type: :object,
-  #     #              properties: {
-  #     #                id: { type: :integer },
-  #     #                name: { type: :string },
-  #     #                created_at: { type: :string },
-  #     #                updated_at: { type: :string }
-  #     #              }
-  #     #       run_test!
-  #     #     end
-  #     #   end
-  #     # end
-  #   end
+    path '/api/categories/{id}' do
+      get 'Retrieves a category' do
+        tags 'Categories'
+        produces 'application/json'
+        parameter name: :id, in: :path, type: :string
+        response '200', 'category found' do
+          schema type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   name: { type: :string },
+                   created_at: { type: :string },
+                   updated_at: { type: :string }
+                 },
+                 required: %w[id name created_at updated_at]
+        end
+      end
+    end
+  end
 end
